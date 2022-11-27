40c16c9c133bb7b3ce298e2c73771e1c1a542f00
nobuddy2743 <92927916+nobuddy2743@users.noreply.github.com>
2022-11-27 12:03:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,14 @@ start_message = """
 <b>👋Hello {}</b>
 <b>I am an AutoCaption bot</b>
 <b>All you have to do is add me to your channel and I will show you my power</b>
-<b>@Mo_Tech_YT</b>"""
+<b>@tgnvs</b>"""
 
 about_message = """
-<b>• Name : [AutoCaption V1](t.me/{username})</b>
+<b>• Name : [AutoCaption Bot](t.me/{username})</b>
 <b>• Developer : [Muhammed](https://github.com/PR0FESS0R-99)
 <b>• Language : Python3</b>
 <b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/Mo_Tech_YT>Click Here</a></b>
-<b>• Source Code : <a href=https://github.com/PR0FESS0R-99/AutoCaption-Bot>Click Here</a></b>"""
+<b>• Updates : <a href=https://t.me/tgnvs>Click Here</a></b>"""
 
 @AutoCaptionBot.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
@@ -76,7 +75,9 @@ def get_file_details(update: pyrogram.types.Message):
 def start_buttons(bot, update):
   bot = bot.get_me()
   buttons = [[
-   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/Mo_Tech_YT"),
+   pyrogram.types.InlineKeyboardButton("❤ Donation Link', url='https://upier.vercel.app/pay/tgnvs@airtel")
+   ],[
+   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/tgnvs"),
    pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
    ],[
    pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
@@ -89,7 +90,7 @@ def about_buttons(bot, update):
    ]]
   return pyrogram.types.InlineKeyboardMarkup(buttons)
 
-print("Telegram AutoCaption V1 Bot Start")
-print("Bot Created By https://github.com/PR0FESS0R-99")
+print("Telegram AutoCaption Bot Bot Start")
+print("Bot Created By TGNVS")
 
 AutoCaptionBot.run()
